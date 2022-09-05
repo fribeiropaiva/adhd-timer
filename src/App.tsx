77e3aef7f0f0c3f417fcccd83bb1b7225d51aa36
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-09-05 09:20:17 -0300

    [adhd-timer] Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobatStyle } from "./styles/themes/global";
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div className="App">Hello World</div>
+      <GlobatStyle />
     </ThemeProvider>
   );
 }
